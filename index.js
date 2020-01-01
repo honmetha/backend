10e36367dcdbab1881b966c89c45a933e7904aff
Hon <dennis.mejen@gmail.com>
2020-01-01 22:24:13 +0700

    Add cors

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express')
 const db = require('./models')
 const bodyParser = require('body-parser')
 const userService = require('./services/user')
+const cors = require('cors')
 
 const app = express()
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( { extended: true }));
+
+app.use(cors())
 
 db.sequelize.sync({ alter:true }).then(()=>{
   userService(app, db)
