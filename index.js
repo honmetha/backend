9953d20a9ab8f5175e2f2e04b7e7ee0c53cfed21
Hon <dennis.mejen@gmail.com>
2020-01-10 00:33:52 +0700

    Update recent

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const bodyParser = require('body-parser')
 const userService = require('./services/user')
 const postService = require('./services/post')
 const cors = require('cors')
+const expressFileUpload = require('express-fileupload')
 
 const app = express()
 
+app.use(expressFileUpload());
+app.use(express.static('uploads'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( { extended: true }));
-
 app.use(cors())
 
 db.sequelize.sync({ alter:true }).then(()=>{
--- services/post.js
+++ services/post.js
@@ -1,6 +1,11 @@
 module.exports = (app, db) => {
   app.get('/getAllPost', (req, res) => {
-    db.post.findAll()
+    db.post.findAll({
+      attributes:[['message','text'],['img', 'imgUrl']],
+      order: [
+        ['id', 'DESC']
+      ]
+    })
     .then(result => {
       res.send(result)
     })
@@ -11,16 +16,35 @@ module.exports = (app, db) => {
   })
 
   app.post('/createPost', (req, res) => {
-    db.post.create({
-      message: req.body.message,
-      img: req.body.img
-    })
-    .then(result => {
-      res.status(201).send(result)
-    })
-    .catch(err => {
-      console.error(err)
-      res.status(400).send({ message: err.message })
-    })
+    if (!req.files) {
+      db.post.create({
+        message: req.body.message,
+      })
+      .then(result => {
+        res.status(201).send(result)
+      })
+      .catch(err => {
+        console.error(err)
+        res.status(400).send({ message: err.message })
+      })
+      res.status(201).send('posted without img')
+    } else {
+      const picture = req.files.postImage
+
+      let pictureName = (new Date()).getTime()
+      picture.mv(`./uploads/${pictureName}.jpeg`)
+      console.log(req.body.message)
+      db.post.create({
+        message: req.body.message,
+        img: `http://localhost:8080/${pictureName}.jpeg`
+      })
+      .then(result => {
+        res.status(201).send(result)
+      })
+      .catch(err => {
+        console.error(err)
+        res.status(400).send({ message: err.message })
+      })
+    }
   })
 }
